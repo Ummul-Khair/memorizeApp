8c33b0848487544d20d9ced7cba247d98c934f27
ummulkhairabdulkarim <ummalabdul@yahoo.com>
2024-04-14 12:12:07 +0100

    Initial Commit

---
+++ memorizeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  memorizeApp
+//
+//  Created by Ummul Khair Abdulkarim on 14/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ memorizeApp/memorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeAppApp.swift
+//  memorizeApp
+//
+//  Created by Ummul Khair Abdulkarim on 14/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct memorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
